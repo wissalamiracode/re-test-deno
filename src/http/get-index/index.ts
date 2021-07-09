1536e21c64a0fb1db790bfa77b7cd56b4582aa61
Wissal Amira <79423488+wissalamiracode@users.noreply.github.com>
2021-07-09 16:03:51 +0200

    Update index.ts

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,26 +1,16 @@
 // TODO: modify the body object!
-let body ="";
-  let isempty=false;
-   for(const[k,v] of Object.entries(event.parameter)){
-     let trimvalues=v.trim();
-     if(trimvalues!== ""){
-       body+=`<br>${k} : ${trimvalues}`;
-     }else{
-       isempty=true;
-       break;
-     }
-   }
+
   export async function handler (req: object) {
-    if(! isempty){
+   
   return {
     statusCode: 200,
     headers: {
       'content-type': 'text/html; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
-    body
+    body:JSON.stringify(req)
   }
-  }
+  
 }
 
 // Example responses
