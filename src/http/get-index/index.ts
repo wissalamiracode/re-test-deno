47c2f352ba4d7fcec063101ae118e0c729b435ca
Wissal Amira <79423488+wissalamiracode@users.noreply.github.com>
2021-07-11 19:29:53 +0200

    Update index.ts

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -7,7 +7,8 @@
     headers: {
       'content-type': 'text/html; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-     ' Access-Control-Allow-Origin':'*'
+     Access-Control-Allow-Origin:*
+      Access-Control-Allow-Methods: POST
     },
     body:JSON.stringify(req)
   }
